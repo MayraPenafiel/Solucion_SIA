69587b8f467f2fa4580a33cd17135fb146594f93
MatiusVidal <89400953+MatiusVidal@users.noreply.github.com>
2022-11-13 18:43:06 -0500

    crear ventana registro

--- app/src/main/java/com/example/subastainversaapp/MainActivity.java
+++ app/src/main/java/com/example/subastainversaapp/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.subastainversaapp;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.subastainversaapp.activity.ActivityRecuperaC;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtOlvidar_contrasena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtOlvidar_contrasena=findViewById(R.id.txtOlvidar_contrasena);
+
+        txtOlvidar_contrasena.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //METODO PARA CAMBIAR VENTANA
+                Intent recuperarC = new Intent(MainActivity.this, ActivityRecuperaC.class);
+                startActivity(recuperarC);
+                finish();
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/example/subastainversaapp/activity/ActivityRecuperaC.java
@@ -0,0 +1,15 @@
+package com.example.subastainversaapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.subastainversaapp.R;
+
+public class ActivityRecuperaC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recupera_c);
+
+    }
+}
