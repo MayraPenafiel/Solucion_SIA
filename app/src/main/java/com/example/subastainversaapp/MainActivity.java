52453faf4ddf6b80cea36a080e40316131ea8353
MatiusVidal <89400953+MatiusVidal@users.noreply.github.com>
2022-11-13 22:06:42 -0500

    cambos main

--- app/src/main/java/com/example/subastainversaapp/MainActivity.java
+++ app/src/main/java/com/example/subastainversaapp/MainActivity.java
@@ -1,33 +1,29 @@
 package com.example.subastainversaapp;
 
 import android.content.Intent;
-import android.view.View;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import com.example.subastainversaapp.activity.ActivityRecuperaC;
+import android.os.Handler;
+import com.example.subastainversaapp.activity.ActivityLogin;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView txtOlvidar_contrasena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtOlvidar_contrasena=findViewById(R.id.txtOlvidar_contrasena);
 
-        txtOlvidar_contrasena.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //METODO PARA CAMBIAR VENTANA
-                Intent recuperarC = new Intent(MainActivity.this, ActivityRecuperaC.class);
-                startActivity(recuperarC);
-                finish();
-            }
-        });
     }
+
+        public void run() {
+            Intent intent= new Intent(MainActivity.this, ActivityLogin.class);
+            startActivity(intent);
+            finish();
+        }
+
 
 
 }
---
+++ app/src/main/java/com/example/subastainversaapp/activity/ActivityLogin.java
@@ -0,0 +1,32 @@
+package com.example.subastainversaapp.activity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.subastainversaapp.MainActivity;
+import com.example.subastainversaapp.R;
+
+public class ActivityLogin extends AppCompatActivity {
+
+    TextView txtOlvidar_contrasena;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        txtOlvidar_contrasena=findViewById(R.id.txtOlvidar_contrasena);
+
+        txtOlvidar_contrasena.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //METODO PARA CAMBIAR VENTANA
+                /*Intent recuperarC = new Intent(ActivityLogin.this, ActivityRecuperaC.class);
+                startActivity(recuperarC);
+                finish();*/
+                startActivity(new Intent(ActivityLogin.this, ActivityRecuperaC.class));
+            }
+        });
+    }
+}
