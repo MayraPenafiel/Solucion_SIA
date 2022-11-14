20c435b3e9b7a28fd05dbe4ca06422b98f2ef360
MatiusVidal <89400953+MatiusVidal@users.noreply.github.com>
2022-11-13 20:37:15 -0500

    ventanas de dialogo

--- app/src/main/java/com/example/subastainversaapp/activity/ActivityRecuperaC.java
+++ app/src/main/java/com/example/subastainversaapp/activity/ActivityRecuperaC.java
@@ -1,15 +1,28 @@
 package com.example.subastainversaapp.activity;
 
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.FragmentManager;
 import com.example.subastainversaapp.R;
 
 public class ActivityRecuperaC extends AppCompatActivity {
+
+    Button btn_enviar_codigo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recupera_c);
-
+        btn_enviar_codigo = (Button) findViewById(R.id.btn_enviar_codigo);
+        btn_enviar_codigo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getSupportFragmentManager();
+                DialogoCorreoV dc = new DialogoCorreoV();
+                dc.show(fm, "tagAlerta");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/subastainversaapp/activity/DialogoCorreoV.java
@@ -0,0 +1,26 @@
+package com.example.subastainversaapp.activity;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.fragment.app.DialogFragment;
+import com.example.subastainversaapp.R;
+
+public class DialogoCorreoV extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder= new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.activity_dialogo_correo_v, null)).setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        return builder.create();
+    }
+}
