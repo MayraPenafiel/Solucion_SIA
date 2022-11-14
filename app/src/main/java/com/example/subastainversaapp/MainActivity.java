cdb0fa85b26f907a87ea9b32e8f388ccc40ceed3
MatiusVidal <89400953+MatiusVidal@users.noreply.github.com>
2022-11-13 22:15:36 -0500

    animacion

--- app/src/main/java/com/example/subastainversaapp/MainActivity.java
+++ app/src/main/java/com/example/subastainversaapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.subastainversaapp;
 
 import android.content.Intent;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,16 +18,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Animation animacion1= AnimationUtils.loadAnimation( this,R.anim.desplazamiento_arriba);
+        //ImageView logo_galeria= findViewById(R.id.logo_galeria);
+        //logo_galeria.setAnimation(animacion1);
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,ActivityLogin.class);
+                startActivity(intent);
+                finish();
 
+            }
+        }, 3000);
     }
-
-        public void run() {
-            Intent intent= new Intent(MainActivity.this, ActivityLogin.class);
-            startActivity(intent);
-            finish();
-        }
-
-
-
 }
