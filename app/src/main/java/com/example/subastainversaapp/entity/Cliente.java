c3faf7d896adb78ff6144827e53af722f1c92da1
Bryan Cueva <axelbawy@gmail.com>
2022-11-18 19:21:47 -0500

    creacion de cliente en api 100%

--- app/src/main/java/com/example/subastainversaapp/entity/Cliente.java
+++ app/src/main/java/com/example/subastainversaapp/entity/Cliente.java
@@ -5,47 +5,88 @@ import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
 
 public class Cliente implements Serializable {
-    @SerializedName("id_cliente")
-        private Long id_cliente;
+
     @SerializedName("id_persona")
-        private String id_persona;
+    private Long id_persona;
+    @SerializedName("nombre")
+    private String nombre;
+    @SerializedName("apellido")
+    private String apellido;
+    @SerializedName("email")
+    private String email;
+    @SerializedName("telefono")
+    private String telefono;
+    @SerializedName("direccion")
+    private String direccion;
+    @SerializedName("usuario")
+    Usuario usuario = new Usuario();
 
-    @SerializedName("usuario_id")
-    private String usuario_id;
-
-    public Cliente(Long id_cliente, String id_persona, String usuario_id) {
-        this.id_cliente = id_cliente;
+    public Cliente(Long id_persona, String nombre, String apellido, String email, String telefono, String direccion, Usuario usuario) {
         this.id_persona = id_persona;
-        this.usuario_id = usuario_id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.usuario = usuario;
     }
-
-    public String getUsuario_id() {
-        return usuario_id;
+    public Cliente(Long id_persona, String nombre, String apellido, String email, String telefono, String direccion, String usuario) {
     }
-
-    public void setUsuario_id(String usuario_id) {
-        this.usuario_id = usuario_id;
-    }
-
-    public String getId_persona() {
+    public Long getId_persona() {
         return id_persona;
     }
 
-    public void setId_persona(String id_persona) {
+    public void setId_persona(Long id_persona) {
         this.id_persona = id_persona;
     }
 
-    public Cliente() {
-        }
-
-        public Long getId_cliente() {
-            return id_cliente;
-        }
-
-        public void setId_cliente(Long id_cliente) {
-            this.id_cliente = id_cliente;
-        }
-
+    public String getNombre() {
+        return nombre;
     }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
 
 
--- app/src/main/java/com/example/subastainversaapp/repository/ServiceCliente.java
+++ app/src/main/java/com/example/subastainversaapp/repository/ServiceCliente.java
@@ -1,2 +1,11 @@
-package com.example.subastainversaapp.repository;public interface ServiceCliente {
+package com.example.subastainversaapp.repository;
+
+import com.example.subastainversaapp.entity.Cliente;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface ServiceCliente {
+    @POST("/cliente/crear")
+    Call<Cliente> createPost(@Body Cliente cliente);
 }
