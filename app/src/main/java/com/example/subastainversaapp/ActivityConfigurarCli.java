932a1752d5e63654a8eb4bd468136949a08e3669
MatiusVidal <89400953+MatiusVidal@users.noreply.github.com>
2022-11-18 23:49:40 -0500

    Crear activity configuracion cliente y provee

---
+++ app/src/main/java/com/example/subastainversaapp/ActivityConfigurarCli.java
@@ -0,0 +1,13 @@
+package com.example.subastainversaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityConfigurarCli extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configurar_cli);
+    }
+}
---
+++ app/src/main/java/com/example/subastainversaapp/DiagoloAdvertenciaCli.java
@@ -0,0 +1,10 @@
+package com.example.subastainversaapp;
+
+import android.app.DialogFragment;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiagoloAdvertenciaCli extends DialogFragment {
+
+
+}
