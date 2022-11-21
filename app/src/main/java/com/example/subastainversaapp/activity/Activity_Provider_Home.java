80e7543f107fa77b75c75da986adcf01cbe47701
MatiusVidal <89400953+MatiusVidal@users.noreply.github.com>
2022-11-20 22:02:38 -0500

    cambios

--- app/src/main/java/com/example/subastainversaapp/activity/Activity_Provider_Home.java
+++ app/src/main/java/com/example/subastainversaapp/activity/Activity_Provider_Home.java
@@ -1,16 +1,22 @@
 package com.example.subastainversaapp.activity;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.subastainversaapp.ActivityConfigurarCli;
+import com.example.subastainversaapp.ActivityConfigurarPro;
+import com.example.subastainversaapp.ActivityServicios;
 import com.example.subastainversaapp.R;
 import com.example.subastainversaapp.activity.Activity_Provider_Home;
 
 import java.util.List;
 
 public class Activity_Provider_Home extends AppCompatActivity {
+
+    Button btn_configuracion;
 
     Activity_Provider_Home binding;
     private ListView saleList;
@@ -23,9 +29,26 @@ public class Activity_Provider_Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider_home);
         saleList= (ListView) findViewById(R.id.listSale);
+        btn_configuracion= (Button) findViewById(R.id.btn_configuracion);
 
         ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, R.layout.sale_list_item, nombres);
         saleList.setAdapter(adapter);
+
+        //MOSTRARA LA VENTANA PARA CONFIGURAR CLIENTE
+        btn_configuracion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Activity_Provider_Home.this, ActivityConfigurarCli.class));
+            }
+        });
+        //MOSTRARA LA VENTANA PARA CONFIGURAR PROVEDOR
+        /*btn_configuracion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Activity_Provider_Home.this, ActivityConfigurarPro.class));
+            }
+        });*/
+
 
 
     }
