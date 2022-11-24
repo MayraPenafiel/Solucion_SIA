9dc2b1fb953afbe263b3eef84dfe8705c88763f2
MatiusVidal <89400953+MatiusVidal@users.noreply.github.com>
2022-11-23 21:36:17 -0500

    subasta calificada

---
+++ app/src/main/java/com/example/subastainversaapp/ActivitySubastaporCalificar.java
@@ -0,0 +1,13 @@
+package com.example.subastainversaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySubastaporCalificar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subastapor_calificar);
+    }
+}
