aa3b7df14a100ee2cac8a7ed516774faa92ff30f
MatiusVidal <89400953+MatiusVidal@users.noreply.github.com>
2022-11-26 00:39:38 -0500

    Update SearchDBHelper.java

--- app/src/main/java/com/example/subastainversaapp/sqlite/SearchDBHelper.java
+++ app/src/main/java/com/example/subastainversaapp/sqlite/SearchDBHelper.java
@@ -1,5 +1,15 @@
 package com.example.subastainversaapp.sqlite;
 
-public class SearchDBHelper {
+import android.content.Context;
+import androidx.annotation.Nullable;
 
+public class SearchDBHelper extends DBHelper{
+
+
+    Context context;
+
+    public SearchDBHelper(@Nullable @org.jetbrains.annotations.Nullable Context context) {
+        super(context);
+        this.context=context;
+    }
 }
