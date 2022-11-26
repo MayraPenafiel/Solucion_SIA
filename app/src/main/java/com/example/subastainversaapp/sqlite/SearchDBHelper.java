e866aaedd3218d2c8eb34c6edde4c3d32f797fa7
MatiusVidal <89400953+MatiusVidal@users.noreply.github.com>
2022-11-26 01:31:59 -0500

    Update SearchDBHelper.java

--- app/src/main/java/com/example/subastainversaapp/sqlite/SearchDBHelper.java
+++ app/src/main/java/com/example/subastainversaapp/sqlite/SearchDBHelper.java
@@ -1,6 +1,8 @@
 package com.example.subastainversaapp.sqlite;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import androidx.annotation.Nullable;
 
 public class SearchDBHelper extends DBHelper{
@@ -11,5 +13,17 @@ public class SearchDBHelper extends DBHelper{
     public SearchDBHelper(@Nullable @org.jetbrains.annotations.Nullable Context context) {
         super(context);
         this.context=context;
+    }
+
+    public Cursor consultar (String nombre){
+        DBHelper dbHelper = new DBHelper(context);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String sql = "Select * from t_usuario";
+        String[] selectionArgs = null;
+        if (nombre != null) {
+            sql += " where name LIKE '%' || ? || '%'";
+            selectionArgs = new String[] {nombre};
+        }
+        return db.rawQuery(sql, selectionArgs);
     }
 }
