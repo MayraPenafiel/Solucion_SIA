a4ef6025f8cc44b88d0ef7a6f4de1b78e492c38e
Jey <89442351+MayraPenafiel@users.noreply.github.com>
2022-11-27 02:25:59 -0500

    borra clases innecesarias

--- app/src/main/java/com/example/subastainversaapp/repository/ServiceOferta.java
+++ app/src/main/java/com/example/subastainversaapp/repository/ServiceOferta.java
@@ -1,10 +1,16 @@
 package com.example.subastainversaapp.repository;
 
 import com.example.subastainversaapp.entity.Oferta;
-import retrofit2.Call;
-import retrofit2.http.*;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface ServiceOferta {
     @GET("/oferta/listar")
