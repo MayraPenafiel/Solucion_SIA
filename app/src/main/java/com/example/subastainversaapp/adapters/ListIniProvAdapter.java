bd052976d473826f3e12fd6b99e93b7c43b5764b
Jey <89442351+MayraPenafiel@users.noreply.github.com>
2022-11-27 23:24:03 -0500

    Arreglo errores

--- app/src/main/java/com/example/subastainversaapp/adapters/ListIniProvAdapter.java
+++ app/src/main/java/com/example/subastainversaapp/adapters/ListIniProvAdapter.java
@@ -16,8 +16,8 @@ import java.util.List;
 public class ListIniProvAdapter extends RecyclerView.Adapter<ListIniProvAdapter.ViewHolder> {
 
     private List<Subasta> auctionList;
-    private LayoutInflater layoutInflater;
-    private Context context;
+    private final LayoutInflater layoutInflater;
+    private final Context context;
 
 
 
@@ -78,7 +78,7 @@ public class ListIniProvAdapter extends RecyclerView.Adapter<ListIniProvAdapter.
             //SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
             //String f_ini=formato.format(item.getFechaInicio());
             //String f_fin=formato.format(item.getFechaFin());
-            numero.setText(nro+"");
+            //numero.setText(nro+"");
             servicio.setText(item.getServicio().getNombreServicio());
             //fcini.setText(f_ini);
             //fcfin.setText(f_fin);
